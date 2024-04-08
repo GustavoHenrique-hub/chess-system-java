10c39e15b2b78195aaf2b2489224411c46cfed73
Gustavo Henrique da Silva Santos <gustavosilvah37@gmail.com>
2024-04-08 19:30:51 -0300

    feat: start to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,21 @@
+package boardgame;
+
+public abstract class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+		
+	}
+
+	public Piece(Board board) {
+		this.board = board;
+	}
+	
+	protected Board getBoard() {
+		return board;
+	}
+
+	public abstract Piece[][] possibleMoves();
+}
